6c6d7e691f3c1c0976a837e9bc3e4de1bf2d8b95
marupuro <sutepuku@gmail.com>
2020-07-05 11:12:39 +0000

    destroy直した

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task,only: [:show,:edit,:update,:destroy]
-  before_action :require_user_logged_in
-  before_action :correct_user,only: [:destroy,:edit,:show]
-  
+  before_action :require_user_logged_in,only:[:index,:show]
+  before_action :correct_user,only: [:destroy,:edit,:show,:update]
  def index
    @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(5)
  end
@@ -50,11 +48,7 @@ class TasksController < ApplicationController
  end
  
  private
- 
- def set_task
-   @task = Task.find(params[:id])
- end
- 
+
  
  def task_params
    params.require(:task).permit(:content, :status)
